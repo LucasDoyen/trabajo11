c0375433e294269ddb2092a4b503c1ecf0b6068d
LucasDoyen <lucass.m.d@hotmail.com>
2022-09-18 23:34:41 -0300

    hola

---
+++ variable.js
@@ -0,0 +1,5 @@
+let value = prompt("¿Cuantas veces queres que aparesca Hola Mundo??");
+
+for (let i = 0; i < value ; i++) {
+  document.write("Hola Mundo: " + i + "<br>"); 
+}
---
+++ variables.js
@@ -0,0 +1,46 @@
+let diaSemana = prompt("¿Qué día es hoy?");
+switch (diaSemana) {
+case "lunes":
+
+alert("Hoy es lunes.");
+
+break;
+case "martes":
+
+alert("Hoy es martes.");
+
+break;
+case "miércoles":
+
+alert("Hoy es miércoles.");
+
+break;
+case "jueves":
+
+alert("Hoy es jueves.");
+
+break;
+case "viernes":
+
+alert("Hoy es viernes.");
+
+break;
+case "sábado":
+
+alert("Hoy es sábado.");
+
+break;
+case "domingo":
+
+alert("Hoy es domingo.");
+
+break;
+default:
+
+alert("Escribe el día de la semana en minúsculas.");} 
+
+for(i=20;i<=70;i++){
+
+    document.write("El número es: " + i + "<br>");
+    
+    }
